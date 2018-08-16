d1c629b0f2711a194bd17bba351a45c631f0912d
TX-9 <jacobleecs@gmail.com>
2018-08-16 14:36:38 -0400

    add UI

--- server/server.js
+++ server/server.js
@@ -19,9 +19,9 @@ io.on('connection', (socket) => {
     
     socket.broadcast.emit('newMessage',  generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', generateMessage(message.from, message.text));
-
+    socket.on('createMessage', (message, callback) => {
+        console.log('createMessage', message);
+        callback('This is from server');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
